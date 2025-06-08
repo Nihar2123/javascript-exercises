3250250a97396cbb976d9894169fa0e3425798bb
Nihar2123 <maddilinihar@gmail.com>
2025-06-08 12:00:55 +0530

    Complete JS excercise: 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0) return 'ERROR';
+    if(a != Math.floor(a) || b != Math.floor(b)) return 'ERROR';
+    if(typeof a != 'number' || typeof b != 'number') return 'ERROR';
 
+    let sum = 0;
+    if(a > b) [a, b] = [b, a];
+    sum = b*(b + 1)/2 - a*(a - 1)/2;
+    return sum;
 };
 
 // Do not edit below this line
