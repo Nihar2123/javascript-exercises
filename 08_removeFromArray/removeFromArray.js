4db8132920820a4cb35e0e359b3d5cde7a8978ff
Nihar2123 <maddilinihar@gmail.com>
2025-06-08 11:32:28 +0530

    Complete JS excercise: 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    // for(let val of values){
+    //     for (let i = 0; i < arr.length; i++){
+    //         if (arr[i] === val){
+    //             arr.splice(i, 1);
+    //             i--;
+    //         }
+    //     }
+    // }
+
+    //return arr;
+
+    return arr.filter(val => !values.includes(val));
 };
 
 // Do not edit below this line
