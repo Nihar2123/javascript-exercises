0c9bf517df2e87699fef9744578d4962efa089b1
Nihar2123 <maddilinihar@gmail.com>
2025-06-08 11:15:50 +0530

    Complete JS excercise: 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0) return 'ERROR';
+    let strAns = '';
+    while(num--){
+        strAns += str;
+    }
+    return strAns;
 };
 
 // Do not edit below this line
