e4a75f1b919e24aa4ba3b7a34413d5dc4ae28df5
Nihar2123 <maddilinihar@gmail.com>
2025-06-09 10:15:57 +0530

    Complete JS excercise: 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
+    if(n < 0) return 'OOPS';
+    let a = 0, b = 1, c;
+    for(let i = 1; i <= n; i++) {
+        c = a + b;
+        a = b; b = c;
+    }
 
+    return a;
 };
 
 // Do not edit below this line
