5bc6156952b728737a1662eb12d420b5c45b047c
Nihar2123 <maddilinihar@gmail.com>
2025-06-09 10:12:22 +0530

    Complete JS excercise: 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let i = 0, j = str.length - 1;
+    while(i < j){
+        while(!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9')) ){i++;}
+        while(!((str[j] >= 'a' && str[j] <= 'z') || (str[j] >= '0' && str[j] <= '9')) ){j--;}
+        if(i >= j) return true;
+        if(str[i++] !== str[j--]) return false;
+    }
 
+    return true;
 };
 
 // Do not edit below this line
