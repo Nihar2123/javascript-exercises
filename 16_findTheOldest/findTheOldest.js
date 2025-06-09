c22f09975353052059a6c04707525c282ddb05d2
Nihar2123 <maddilinihar@gmail.com>
2025-06-09 10:32:24 +0530

    Complete JS excercise: 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currYear = new Date().getFullYear();
+    let ourGuy;
+    people.reduce((oldest, current) => {
+        let death;
+        if(!current.yearOfDeath) death = currYear;
+        else death = current.yearOfDeath;
+        let age = death - current.yearOfBirth;
+        if(age > oldest){
+            oldest = age;
+            ourGuy = current;
+        }
+        return oldest;
+    }, 0);
 
+    return ourGuy;
 };
 
 // Do not edit below this line
